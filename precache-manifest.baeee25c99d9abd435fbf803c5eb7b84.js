55574e6d17ed1cbd9950c792fc32ac51ef91ee63
kniferdev <71694770+kniferdev@users.noreply.github.com>
2022-07-05 15:24:56 +0800

    Updates

---
+++ precache-manifest.baeee25c99d9abd435fbf803c5eb7b84.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ad0ba567a3418a23ac6",
+    "url": "/robofriends/static/css/main.18c2b21b.chunk.css"
+  },
+  {
+    "revision": "4ad0ba567a3418a23ac6",
+    "url": "/robofriends/static/js/main.4ad0ba56.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/robofriends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "4ac91dc872c3d3e535b1",
+    "url": "/robofriends/static/css/2.619a3c13.chunk.css"
+  },
+  {
+    "revision": "4ac91dc872c3d3e535b1",
+    "url": "/robofriends/static/js/2.4ac91dc8.chunk.js"
+  },
+  {
+    "revision": "69d7087b8b0ba8fee442",
+    "url": "/robofriends/static/js/runtime~main.69d7087b.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/robofriends/static/media/SEGA.82e09f78.TTF"
+  },
+  {
+    "revision": "e9e1aec3d1241dd3106fa9fda9166923",
+    "url": "/robofriends/index.html"
+  }
+];
